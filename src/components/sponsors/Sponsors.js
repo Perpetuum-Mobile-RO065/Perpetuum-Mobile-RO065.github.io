7fc945a4bd1f57f917f7e72f6eba2482f538080f
AleXutzZu <davidalex_robert@yahoo.com>
2022-06-26 13:02:54 +0300

    Created some placeholder components for the rest of the routes

---
+++ src/components/sponsors/Sponsors.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Sponsors() {
+    return (
+        <div>
+            <h1>This is the sponsors page</h1>
+            <p>Lorem ipsum smth</p>
+        </div>
+    )
+}
