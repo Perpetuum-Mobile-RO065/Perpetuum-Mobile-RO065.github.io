a8b4157b8cd0bc1bd03fa187e07df3e84044cf7c
AleXutzZu <davidalex_robert@yahoo.com>
2022-07-29 19:13:40 +0300

    Updated VolunteeringForm.js to fit the design pattern of the contact form

--- src/components/form/volunteering/VolunteeringForm.js
+++ src/components/form/volunteering/VolunteeringForm.js
@@ -1,41 +1,86 @@
-import React from "react";
-import {useState} from "react";
-import {useNavigate} from "react-router-dom";
-import emailjs from "@emailjs/browser";
+import React, {useMemo, useState} from "react";
+import useEmail from "../../../hooks/email/useEmail";
+import * as Yup from "yup";
+import {Form, Formik} from "formik";
+import ReCAPTCHA from "react-google-recaptcha";
+import TextInput from "../fields/text/TextInput";
+import Radio from "../fields/selectors/Radio";
+import TextArea from "../fields/text/TextArea";
 
 export default function VolunteeringForm() {
-    const [formData, setFormData] = useState({user_name: "", user_email: "", message: ""})
-    const navigate = useNavigate();
+    const [captcha, setCaptcha] = useState(false);
+    const email = useEmail(process.env.REACT_APP_EMAILJS_SERVICE_ID, process.env.REACT_APP_EMAILJS_TEMPLATE_ID_VOLUNTEER, process.env.REACT_APP_EMAILJS_PUBLIC_KEY);
 
-    const formChange = (event) => {
-        const {value, name} = event.target;
-        setFormData(prevState => {
-            return {...prevState, [name]: value};
-        })
+    const handleCaptchaChange = (value) => {
+        if (value === null) {
+            setCaptcha(false);
+        } else {
+            setCaptcha(true);
+        }
     }
 
-    const sendFakeEmail = (event) => {
-        event.preventDefault();
-        setTimeout(() => navigate("../success", {replace: true, state: {email: formData.user_email, type: "volunteering"}}), 1000);
-    }
-
-    const sendEmail = (event) => {
-        event.preventDefault();
-        emailjs.send("service_nm6x8na", "template_owcu8ll", formData, "CS8FmbKAqyI6-414J")
-            .then(value => {
-                navigate("../success", {replace: true, state: formData})
-            }, error => console.log(error.text))
-    }
+    const formSchema = useMemo(() => {
+        return Yup.object({
+            lastName: Yup.string().required("Last name is required"),
+            firstName: Yup.string().required("First name is required"),
+            email: Yup.string().email("Email is invalid").required("Email is required"),
+            phone: Yup.number(),
+            school: Yup.string().required("School is required"),
+            discord: Yup.string().matches(/^.{3,32}#\d{4}$/, "Discord ID is invalid"),
+            role: Yup.string().required("Role is required"),
+            department: Yup.string().required("Department is required"),
+            hobbies: Yup.string().max(1000, "Hobbies must be less than 1000 characters"),
+            qualifications: Yup.string().required("Qualifications are required"),
+            message: Yup.string().max(1000, "Message must be less than 1000 characters"),
+        });
+    }, []);
 
     return (
-        <form onSubmit={sendFakeEmail}>
-            <label>Name</label>
-            <input type="text" name="user_name" onChange={formChange} value={formData.user_name}/>
-            <label>Email</label>
-            <input type="email" name="user_email" onChange={formChange} value={formData.user_email}/>
-            <label>Message</label>
-            <textarea name="message" onChange={formChange} value={formData.message}/>
-            <input type="submit" value="Send"/>
-        </form>
+        <Formik initialValues={{
+            firstName: "",
+            lastName: "",
+            email: "",
+            phone: "",
+            school: "",
+            discord: "",
+            role: "",
+            department: "",
+            hobbies: "",
+            qualifications: "",
+            message: "",
+        }} validationSchema={formSchema}
+                onSubmit={(values) => {
+                    email(values);
+                }
+                }>
+            {({isSubmitting,}) => <Form>
+                <TextInput label="First name" name="firstName" type="text" placeholder="John"/>
+                <TextInput label="Last name" name="lastName" type="text" placeholder="Doe"/>
+                <TextInput label="Email" name="email" type="email" placeholder="email@example.com"/>
+                <TextInput label="Phone Number" name="phone" type="number" placeholder=""/>
+                <TextInput label="School" name="school" type="text" placeholder="School"/>
+                <TextInput label="Discord" name="discord" type="text" placeholder=""/>
+                <Radio label="Role" name="role">
+                    <option value="">Select Role</option>
+                    <option value="Member">Member</option>
+                    <option value="Volunteer">Volunteer</option>
+                </Radio>
+
+                <Radio label="Department" name="department">
+                    <option value="">Select Department</option>
+                    <option value="Programare">Programare</option>
+                    <option value="Proiectare 3D">Proiectare 3D</option>
+                    <option value="Mecanica">Mecanica</option>
+                    <option value="PR">Public Relations (PR)</option>
+                    <option value="Caiet tehnic">Caiet tehnic</option>
+                </Radio>
+
+                <TextArea label="Hobbies" name="hobbies" placeholder="Hobbies"/>
+                <TextArea label="Qualifications" name="qualifications" placeholder="Qualifications"/>
+                <TextArea label="Message" name="message" placeholder="Message"/>
+                <button type="submit" disabled={isSubmitting || !captcha}>Submit</button>
+                <ReCAPTCHA sitekey={process.env.REACT_APP_RECAPTCHA_SITE_KEY} onChange={handleCaptchaChange}/>
+            </Form>}
+        </Formik>
     )
 }
