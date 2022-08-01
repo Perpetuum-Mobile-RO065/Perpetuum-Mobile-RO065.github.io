0ce4b97e12a64c54e1e7c97e4507fa9f65ed0d6d
AleXutzZu <davidalex_robert@yahoo.com>
2022-08-01 20:46:58 +0300

    Fixed class names to avoid conflicts

--- src/components/form/contact/ContactForm.js
+++ src/components/form/contact/ContactForm.js
@@ -27,53 +27,53 @@ export default function ContactForm() {
     });
 
     return (
-        <div className={"form-template"}>
-            <div className="contact-info">
+        <div className={"contact-form-template"}>
+            <div className="contact-contact-info">
                 <h1>Formular de contact</h1>
-                <hr className={"divider"}/>
-                <div className="form_text">
+                <hr className={"contact-divider"}/>
+                <div className="contact-form_text">
                     <br/>
-                    <p className="margin-5px">Doriți să ne contactați?</p>
-                    <p className="margin-5px">Aici ne puteți scrie un email.</p>
+                    <p className="contact-margin-5px">Doriți să ne contactați?</p>
+                    <p className="contact-margin-5px">Aici ne puteți scrie un email.</p>
                     <br/>
                     <br/>
-                    <div className="email_section">
-                        <img alt="Icon for the email" className="location_icon"
+                    <div className="contact-email_section">
+                        <img alt="" className="contact-location_icon"
                              src="/icons/form-icon-location.png"/>
-                        <p className="email_text margin-5px">Strada Mihai Eminescu 5, Satu Mare 440014, SM,
+                        <p className="contact-email_text contact-margin-5px">Strada Mihai Eminescu 5, Satu Mare 440014, SM,
                             România</p>
                     </div>
-                    <div className="email_section">
-                        <img alt="Icon for the location" className="email_icon"
+                    <div className="contact-email_section">
+                        <img alt="" className="email_icon"
                              src="/icons/form-icon-email.png"/>
-                        <p className="margin-5px">perpetuum.mobile@eminescusm.ro</p>
+                        <p className="contact-margin-5px">perpetuum.mobile@eminescusm.ro</p>
                     </div>
                     <br/><br/>
-                    <p className="margin-5px">Nu sunteți siguri de locație? </p>
+                    <p className="contact-margin-5px">Nu sunteți siguri de locație? </p>
                     <a href="https://www.google.com/maps/place/Colegiul+Na%C8%9Bional+Mihai+Eminescu+Satu+Mare/@47.7893913,22.8741361,17z/data=!3m1!4b1!4m5!3m4!1s0x473805cdaac2883b:0x49b9e88a38ed23fe!8m2!3d47.7893834!4d22.8763575"
-                       target="_blank" className="maps-link">Ne găsiți pe Google Maps!</a>
+                       target="_blank" className="contact-maps-link" rel="noreferrer">Ne găsiți pe Google Maps!</a>
                 </div>
             </div>
 
             <Formik initialValues={{userName: "", email: "", message: "", title: ""}} validationSchema={formSchema}
                     onSubmit={values => email(values)}>
                 {({isSubmitting}) =>
-                    <Form className={"form"}>
+                    <Form className={"contact-form"}>
                         <div>
                             <TextInput label="Nume complet" name="userName" type="text" placeholder="Nume Prenume"
-                                       labelClass="input-label" inputClass="input-box"/>
+                                       labelClass="contact-input-label" inputClass="contact-input-box"/>
                             <TextInput label="Email" name="email" type="email" placeholder="email@example.com"
-                                       labelClass="input-label" inputClass="input-box"/>
+                                       labelClass="contact-input-label" inputClass="contact-input-box"/>
                             <TextInput label="Titlu" name="title" type="text" placeholder="Titlul mesajului"
-                                       labelClass="input-label" inputClass="input-box"/>
+                                       labelClass="contact-input-label" inputClass="contact-input-box"/>
                             <TextArea label="Mesaj" name="message" type="text"
-                                      placeholder="Bună ziua! Vă contactez în legătură cu..." labelClass="input-label"
-                                      inputClass="input-box message"/>
+                                      placeholder="Bună ziua! Vă contactez în legătură cu..." labelClass="contact-input-label"
+                                      inputClass="contact-input-box message"/>
                         </div>
-                        <div className="two-buttons">
+                        <div className="contact-send">
                             <ReCAPTCHA sitekey={process.env.REACT_APP_RECAPTCHA_SITE_KEY}
                                        onChange={handleCaptchaChange}/>
-                            <button type="submit" disabled={(isSubmitting || !captcha)} className="submit">Trimite
+                            <button type="submit" disabled={(isSubmitting || !captcha)} className="contact-submit">Trimite
                             </button>
                         </div>
                     </Form>}
