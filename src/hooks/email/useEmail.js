e43374c6d84bb582dbdb4341c81c033052cfd19a
AleXutzZu <davidalex_robert@yahoo.com>
2022-08-18 11:04:17 +0300

    Fixed typo

--- src/hooks/email/useEmail.js
+++ src/hooks/email/useEmail.js
@@ -1,11 +1,11 @@
 import {useNavigate} from "react-router-dom";
-import emailjs from "@emailjs/browser";
+import emailJS from "@emailjs/browser";
 
 export default function useEmail(serviceID, templateID, publicKey) {
     const navigate = useNavigate();
 
     return (formData) => {
-        emailjs.send(serviceID, templateID, formData, publicKey)
+        emailJS.send(serviceID, templateID, formData, publicKey)
             .then(() => {
                 navigate("../success", {replace: true, state: {email: formData.email}})
             }, e => navigate("../error", {replace: true, state: {error: e.text}})
