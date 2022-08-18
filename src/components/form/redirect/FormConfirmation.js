1d9e860b4f8b3ff93ada7cb97b966c079dc4a847
AleXutzZu <davidalex_robert@yahoo.com>
2022-08-18 20:29:00 +0300

    Updated the form success page

--- src/components/form/redirect/FormConfirmation.js
+++ src/components/form/redirect/FormConfirmation.js
@@ -1,13 +1,21 @@
 import React from "react";
-import {useLocation} from "react-router-dom";
+import {Link, useLocation} from "react-router-dom";
+import './FormConfirmation.css';
 
 export default function FormConfirmation() {
-    const {state} = useLocation();
+    const location = useLocation();
+    const buttonText = (location.pathname.split('/')[1] === "voluntariat" ? "Alatură-te echipei!" : "Contact");
+    const {state} = location;
 
     return (
         <div className="success-section">
-            <p className="success-message">YAY! Mesajul a fost trimis cu succes de pe email-ul introdus!</p>
-            <img alt="Happy robot cheering" src="/images/happy-robot.png"/>
+            <h2 className="success-message">
+                {state ? `YAY! Mesajul a fost trimis cu succes de pe adresa ${state.email}.` :
+                    "HEY! Ai intrat pe această pagină din greșeală."}
+            </h2>
+            <img alt="Happy robot cheering" src={`/images/${state ? "happy-robot" : "map-robot"}.png`} className={"robot-image"}/>
+            {state === null && <Link to={'../'}
+                                     className={"back-button"}>{buttonText}</Link>}
         </div>
     )
 }
