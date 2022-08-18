251f9e42f1c13660ca3c30a9f1adf91bfca6b26a
AleXutzZu <davidalex_robert@yahoo.com>
2022-08-18 16:17:33 +0300

    Updated 404 Page

--- src/components/home/UnknownPage.js
+++ src/components/home/UnknownPage.js
@@ -1,12 +1,18 @@
 import React from "react";
 import './UnknownPage.css';
+import {Link} from "react-router-dom";
 
 export default function UnknownPage() {
     return (
-        <div className="announcement">
-            <h1>404 Page not found</h1>
-            <h2>OOPS!</h2>
-            <h2>Ne pare rău, dar nu am putut găsi pagina pe care o căutai.</h2>
+        <div>
+            <div className="announcement">
+                <img src="/images/sad-robot.png" alt="" className={"sad-robot-image"}/>
+                    <div className="info">
+                        <h1>404 Page not found</h1>
+                        <h2>Ne pare rău, dar nu am putut găsi pagina pe care o căutai.</h2>
+                        <Link to={"/"} className={"back-to-home-link"}>Către pagina principală</Link>
+                    </div>
+            </div>
         </div>
     );
 }
