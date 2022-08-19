2ca94bd656b98f9a8bf37e24dcff134d51441705
AleXutzZu <davidalex_robert@yahoo.com>
2022-08-19 13:03:18 +0300

    Updated the success and error pages to use the new component

--- src/components/form/redirect/FormConfirmation.js
+++ src/components/form/redirect/FormConfirmation.js
@@ -1,21 +1,18 @@
 import React from "react";
-import {Link, useLocation} from "react-router-dom";
+import {useLocation} from "react-router-dom";
 import './FormConfirmation.css';
+import WrongfulRedirect from "./WrongfulRedirect";
 
 export default function FormConfirmation() {
     const location = useLocation();
-    const buttonText = (location.pathname.split('/')[1] === "voluntariat" ? "Alatură-te echipei!" : "Contact");
     const {state} = location;
 
     return (
-        <div className="success-section">
-            <h2 className="success-message">
-                {state ? `YAY! Mesajul a fost trimis cu succes de pe adresa ${state.email}.` :
-                    "HEY! Ai intrat pe această pagină din greșeală."}
-            </h2>
-            <img alt="Happy robot cheering" src={`/images/${state ? "happy-robot" : "map-robot"}.png`} className={"robot-image"}/>
-            {state === null && <Link to={'../'}
-                                     className={"back-button"}>{buttonText}</Link>}
-        </div>
+        state === null ? <WrongfulRedirect/> :
+            <div className="success-section">
+                <h2 className="success-message">YAY! Mesajul a fost trimis cu succes de pe
+                    adresa {state.email}.</h2>
+                <img alt="Happy robot cheering" src="/images/happy-robot.png" className={"robot-image"}/>
+            </div>
     )
 }
--- src/components/form/redirect/FormFailure.js
+++ src/components/form/redirect/FormFailure.js
@@ -1,14 +1,20 @@
 import React from "react";
 import {useLocation} from "react-router-dom";
+import './FormFailure.css';
+import WrongfulRedirect from "./WrongfulRedirect";
 
-export default function FormFailure(){
+export default function FormFailure() {
     const {state} = useLocation();
+    if (state !== null) {
+        console.log(state);
+    }
     return (
-        <>
-            <h1>There has been an error! Please report this to staff</h1>
-            <h2>{state.error}</h2>
-        </>
-
+        state === null ? <WrongfulRedirect/> :
+            <div className="form-failure">
+                <h2 className="fail-message">HMMM... Ceva a mers prost și formularul nu a putut fi trimis...</h2>
+                <img src="/images/gear-robot.png" alt={"Robot holding a gear"} className={"fail-robot-image"}/>
+                <h2 className="success-message">Te rugăm să ne contactezi manual pe adresa de email.</h2>
+            </div>
     )
 
 }
