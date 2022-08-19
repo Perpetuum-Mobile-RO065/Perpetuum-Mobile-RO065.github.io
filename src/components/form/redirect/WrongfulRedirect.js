3dc04eff08a9143355c6438dd74a80644b35aff2
AleXutzZu <davidalex_robert@yahoo.com>
2022-08-19 13:02:47 +0300

    Added a component that will be shown whenever someone navigates to contact/success or other confirmation/error pages without actually having used the service

---
+++ src/components/form/redirect/WrongfulRedirect.js
@@ -0,0 +1,15 @@
+import React from "react";
+import './WrongfulRedirect.css';
+import {Link} from "react-router-dom";
+import useButtonText from "../../../hooks/button/useButtonText";
+
+export default function WrongfulRedirect() {
+    const buttonText = useButtonText();
+    return (
+        <div className={"wrongful-section"}>
+            <h2 className={"wrongful-message"}>HEY! Ai intrat pe această pagină din greșeală.</h2>
+            <img alt="" src="/images/map-robot.png" className={"wrongful-robot-image"}/>
+            <Link to={'../'} className={"back-button"}>{buttonText}</Link>
+        </div>
+    )
+}
