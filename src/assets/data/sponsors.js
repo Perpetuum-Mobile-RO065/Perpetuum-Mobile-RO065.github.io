384218d42bd4d78e0614b690866bf163082f1fc5
AleXutzZu <davidalex_robert@yahoo.com>
2022-08-20 13:14:40 +0300

    Updated sponsors data with some data from last year

---
+++ src/assets/data/sponsors.js
@@ -0,0 +1,75 @@
+const sponsors = [
+    {
+        season: "2021-2022",
+        type: [
+            {
+                name: "Platină",
+                sponsors: [
+                    {
+                        name: "Zollner",
+                        image: "zollner.png",
+                        link: "https://www.zollner.com/",
+                    },
+                ]
+            },
+            {
+                name: "Aur",
+                sponsors: [
+                    {
+                        name: "Agrotex",
+                        image: "agrotex.png",
+                        link: "https://www.zollner.com/",
+                    },
+                    {
+                        name: "AXA STEEL",
+                        image: "axasteel.jpeg",
+                        link: "https://www.zollner.com/",
+                    },
+                    {
+                        name: "AXA COMPANY",
+                        image: "axacompany.jpeg",
+                        link: "https://www.zollner.com/",
+                    },
+                    {
+                        name: "Esedra",
+                        image: "esedra.png",
+                        link: "https://www.zollner.com/",
+                    },
+                ]
+            },
+            /*{
+                name: "Argint",
+                sponsors: [
+                    /!*{
+                        name: "Zollner",
+                        image: "a",
+                        link: "https://www.zollner.com/",
+                    },*!/
+                ]
+            },*/
+           /* {
+                name: "Bronz",
+                sponsors: [
+                    /!*{
+                        name: "Zollner",
+                        image: "a",
+                        link: "https://www.zollner.com/",
+                    },*!/
+                ]
+            }*/
+        ]
+    },
+    {
+        season: "2020-2021",
+    },
+    {
+        season: "2019-2020",
+    },
+    {
+        season: "2018-2019",
+    },
+    {
+        season: "2017-2018",
+    }];
+
+export default sponsors;
