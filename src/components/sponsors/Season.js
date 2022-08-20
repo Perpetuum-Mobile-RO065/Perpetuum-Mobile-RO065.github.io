df61a8ab1f1c8daaf88058c475a705b44c4bcf3d
AleXutzZu <davidalex_robert@yahoo.com>
2022-08-20 13:15:26 +0300

    Started working on laying out the sponsors page and added a bunch of sponsor logos

---
+++ src/components/sponsors/Season.js
@@ -0,0 +1,42 @@
+import React from "react";
+import {Navigate, useParams} from "react-router-dom";
+import sponsors from "../../assets/data/sponsors";
+import './Season.css';
+
+export default function Season() {
+    const params = useParams();
+
+    const searchSeason = (season) => {
+        return sponsors.find(sponsor => sponsor.season === season);
+    }
+
+    const season = searchSeason(params.season);
+    if (season === undefined) {
+        return <Navigate to={"404"} replace={true}/>
+    }
+
+    return (
+        <div className={"sponsors-list"}>
+            {season.type && season.type.map(type => <SponsorType key={type.name} type={type.name} sponsors={type.sponsors} season={season.season}/>)}
+        </div>
+    )
+}
+
+function SponsorType({type, sponsors, season}) {
+    return (
+        <>
+            <h2>Sponsori de {type}</h2>
+            <div className={"logo-section"}>
+                {sponsors && sponsors.map(sponsor => <Sponsor key={sponsor.name} season={season} {...sponsor}/>)}
+            </div>
+        </>
+    )
+}
+
+function Sponsor(props) {
+    return (
+        <div className={"sponsor"}>
+            <img style={{width: "90%"}} src={`/images/sponsors/${props.season}/${props.image}`} alt={props.name}/>
+        </div>
+    )
+}
