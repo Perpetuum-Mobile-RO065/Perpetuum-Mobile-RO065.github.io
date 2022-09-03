3b679809dab6aa849c0048bbe19326dd4b807168
AleXutzZu <davidalex_robert@yahoo.com>
2022-09-03 18:49:51 +0300

    Made the 404 page responsive on mobile devices

--- src/components/home/UnknownPage.js
+++ src/components/home/UnknownPage.js
@@ -4,14 +4,12 @@ import {Link} from "react-router-dom";
 
 export default function UnknownPage() {
     return (
-        <div>
-            <div className="announcement">
-                <img src="/images/robots/sad-robot.png" alt="" className={"sad-robot-image"}/>
-                    <div className="info">
-                        <h1>404 Page not found</h1>
-                        <h2>Ne pare rău, dar nu am putut găsi pagina pe care o căutai.</h2>
-                        <Link to={"/"} className={"back-to-home-link"}>Către pagina principală</Link>
-                    </div>
+        <div className="page-not-found-section">
+            <img src="/images/robots/sad-robot.png" alt="" className={"sad-robot-image"}/>
+            <div className="info">
+                <h1>404 Page not found</h1>
+                <h2>Ne pare rău, dar nu am putut găsi pagina pe care o căutai.</h2>
+                <Link to={"/"} className={"back-to-home-link"}>Către pagina principală</Link>
             </div>
         </div>
     );
