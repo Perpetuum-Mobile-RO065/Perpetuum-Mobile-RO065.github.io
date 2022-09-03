7493487d2c83831316ba6cd23c99170fec00affb
AleXutzZu <davidalex_robert@yahoo.com>
2022-09-03 20:56:49 +0300

    Fixed a bug with emails not displaying nicely on mobile devices

--- src/components/form/redirect/success/FormConfirmation.js
+++ src/components/form/redirect/success/FormConfirmation.js
@@ -7,12 +7,24 @@ export default function FormConfirmation() {
     const location = useLocation();
     const {state} = location;
 
+    if (state === null) return <WrongfulRedirect/>;
+
+    let email = <>{[...state.email].map(char => {
+        if (char === "@" || char === "_" || char === "-" || char === ".") {
+            return (
+                <>{char}
+                    <wbr/>
+                </>
+            );
+        }
+        return <>{char}</>;
+    })}</>;
+
     return (
-        state === null ? <WrongfulRedirect/> :
-            <div className="success-section">
-                <h2 className="success-message">YAY! Mesajul a fost trimis cu succes de pe
-                    adresa {state.email}.</h2>
-                <img alt="Happy robot cheering" src="/images/robots/happy-robot.png" className={"robot-image"}/>
-            </div>
+        <div className="success-section">
+            <h2 className="success-message">YAY! Mesajul a fost trimis cu succes de pe
+                adresa {email}.</h2>
+            <img alt="Happy robot cheering" src="/images/robots/happy-robot.png" className={"robot-image"}/>
+        </div>
     )
 }
