5fc6fed206f5614178f63c2a075fd0f1842a15c0
AleXutzZu <davidalex_robert@yahoo.com>
2022-09-05 17:08:55 +0300

    Disabled compiler warning

--- src/components/form/fields/selectors/Radio.js
+++ src/components/form/fields/selectors/Radio.js
@@ -2,6 +2,7 @@ import React from "react";
 import {useField} from "formik";
 
 export default function Radio({label, inputClass, labelClass, ...props}) {
+    //eslint-disable-next-line
     const [field, meta] = useField(props);
 
     return (
