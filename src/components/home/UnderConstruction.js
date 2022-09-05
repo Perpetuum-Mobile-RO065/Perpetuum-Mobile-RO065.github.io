57502284e12426faa32c80320212d401a9a9ad74
AleXutzZu <davidalex_robert@yahoo.com>
2022-09-05 18:14:34 +0300

    Fixed typo

--- src/components/home/UnderConstruction.js
+++ src/components/home/UnderConstruction.js
@@ -10,7 +10,7 @@ export default function UnderConstruction() {
                 <img src="/images/robots/working-robot.png" alt="Working robot" className="working-robot-image"/>
                 <div className="under-construction-info">
                     <h1>Page in development</h1>
-                    <h2>Ne pare rău, dar momentan muncitorii noștri harnici amenjează această pagină.</h2>
+                    <h2>Ne pare rău, dar momentan muncitorii noștri harnici amenajează această pagină.</h2>
                     <h2>Vă primim cu drag mai târziu!</h2>
                     <Link to={'/'} className="under-construction-link">Către pagina principală</Link>
                 </div>
